ed19db9a916d5eb098d40704c66b35d0aeb4387c
Sashakrm <sasakramch@gmail.com>
2024-12-25 09:42:10 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int count_1 = 0;
+            int count_2 = 0;
+            Random random = new Random();
+            for(int x=0;x <12;x++)
+            {
+                int number = random.Next(-50,50);
+                Console.WriteLine(number);
+                if (number > 0) count_1++;
+                else if (number < 0) count_2++;
+            }
+            Console.WriteLine($"больше нуля: {count_1}");
+            Console.WriteLine($"меньше нуля: {count_2}");
+        }
+    }
+}
