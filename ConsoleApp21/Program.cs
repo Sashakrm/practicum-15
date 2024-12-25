f67202a96ef24acd5349c785cebf6212371d8a11
Sashakrm <sasakramch@gmail.com>
2024-12-25 09:06:35 +0200

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int Count = 0;
+            for(int x=10;x<=99;x++)
+            {
+                if(x%2==0)
+                {
+                    Console.WriteLine(x); Count++;
+                }
+            }
+            Console.WriteLine($"count={Count}");
+        }
+    }
+}
