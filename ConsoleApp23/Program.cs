f98ccd50b184acc100b69b7b845a2456857c918e
Sashakrm <sasakramch@gmail.com>
2024-12-25 09:22:59 +0200

    Add files via upload

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int count = 0;
+            Random random = new Random();
+            for(int x=0; x < 15;x++)
+            {
+                int number = random.Next(-20, 30);
+                Console.WriteLine(number);
+                if (number > 0) count++;
+            }
+            Console.WriteLine($"count={count}");
+        }
+    }
+}
