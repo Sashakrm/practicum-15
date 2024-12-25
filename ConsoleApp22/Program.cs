7d9bd609d61b9c972692b43612127e6caecbff9e
Sashakrm <sasakramch@gmail.com>
2024-12-25 09:15:58 +0200

    Add files via upload

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            Random random = new Random();
+            for (int x = 0; x < 10; x++)
+            {
+                int number = random.Next(-40, 40);
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
