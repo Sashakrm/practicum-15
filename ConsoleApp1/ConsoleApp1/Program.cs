e4c616f4124562ed061d6292ea0c638902c4e507
Sashakrm <sasakramch@gmail.com>
2025-01-05 21:12:03 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+
+        static void Func()
+        {
+            Random random = new Random();
+            for (int x = 0; x < 8; x++)
+            {
+                int number = random.Next(-30, 30);
+                Console.Write(number);
+                if (number > 0)
+                {
+                    Console.Write("-положительное");
+                }
+                else if (number<0)
+                {
+                    Console.Write("-отрицательное");
+                }
+                else 
+                {
+                    Console.WriteLine("нейтральное");
+                }
+
+                if (number != 0)
+                { 
+                    if(number%2==0)
+                    {
+                        Console.WriteLine("-четное");
+                    }
+                    else
+                    {
+                        Console.WriteLine("-нечетное");
+                    }
+                }
+            }
+        }
+    }
+}
