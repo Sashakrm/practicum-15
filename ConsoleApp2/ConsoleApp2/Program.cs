61688f18132bbb31bc04a17746f36518110f2ed7
Sashakrm <sasakramch@gmail.com>
2025-01-05 22:16:30 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+
+        static void Func()
+        {
+            Random random = new Random();
+            for (int x = 0; x < 10; x++)
+            {
+                int number = random.Next(-20, 20);
+                if (number>0)
+                {
+                    Console.Write(number);
+                    if (number%2==0)
+                    {
+                        Console.WriteLine("-четное");
+                    }
+                    else
+                    {
+                        Console.WriteLine("-нечетное");
+                    }
+                }
+            }
+        }
+    }
+}
