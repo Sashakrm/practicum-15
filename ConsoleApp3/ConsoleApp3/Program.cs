694e11a13f4b91ef423d6d74f963ebe875f8491b
Sashakrm <sasakramch@gmail.com>
2025-01-06 11:40:53 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input("До:");
+            Func(n);
+            Console.Read();
+        }
+
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void Func(int n)
+        {
+            int sum = 0;
+            for (int a = 1; a <= n; a++)
+            {
+                sum += a;
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
