630a90c1168bfb798de86c5f84b5f8269a450865
Sashakrm <sasakramch@gmail.com>
2025-01-06 16:38:59 +0200

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+
+        static void Func()
+        {
+            int value = 0;
+            for (int x=10; x <= 99; x++)
+            {
+                if (x %2== 0)
+                {
+                    Console.WriteLine(x);
+                    value++;
+                }
+            }
+            Console.WriteLine($"всего:{value}");
+        }
+    }
+}
