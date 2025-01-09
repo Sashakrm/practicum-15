5e3cad7b30f0d78e5d3999cefc46387b89a0790b
Sashakrm <sasakramch@gmail.com>
2025-01-09 13:10:39 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Func();
+         Console.Read();
+        }
+
+        static void Func()
+        {
+            double price = 20.4;
+            for (int a = 1; a <= 20; a++)
+            {
+                var totalprice = price * a;
+                Console.WriteLine($"{a} товар стоит:{totalprice:F1} рублей");
+            }
+        }
+    }
+}
