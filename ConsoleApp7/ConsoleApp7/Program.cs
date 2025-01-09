2e0ce9cceb123c527a3bb158e5dee7e6e71e1cb5
Sashakrm <sasakramch@gmail.com>
2025-01-09 13:17:25 +0200

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+
+        static void Func()
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                Console.Write("20 ");
+            }
+        }
+    }
+}
